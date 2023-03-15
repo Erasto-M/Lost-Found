8dfe5fc82a2d5b6dadfedcb047ec52c159c59bc3
erastus munyao kyalo <erastusmunya2022@gmail.com>
2023-03-15 21:40:41 +0300

    added the home.dart

---
+++ lib/LostItems/FetchlostItems.dart
@@ -0,0 +1,154 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:lostfound/Authentication/reusableWidgets1.dart';
+class Fetchlost extends StatefulWidget {
+  const Fetchlost({Key? key}) : super(key: key);
+
+  @override
+  State<Fetchlost> createState() => _FetchlostState();
+}
+
+class _FetchlostState extends State<Fetchlost> {
+  final CollectionReference  Lost_items = FirebaseFirestore.instance.collection('Lost_items');
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("All Lost Items",
+        style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+      ),
+      body: Container(
+        margin: const EdgeInsets.only(
+          top: 20,
+          left: 10,
+          right: 10,
+        ),
+        child: StreamBuilder<QuerySnapshot>(
+          stream: Lost_items.snapshots(),
+          builder:(BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot){
+            if(snapshot.hasError){
+              return const Text("There is an error");
+            }
+             if(snapshot.connectionState == ConnectionState.waiting){
+              return const CircularProgressIndicator(
+                color: Colors.red,
+              );
+            }
+            return ListView(
+              children: snapshot.data!.docs.map((DocumentSnapshot document) {
+                return Card(
+                  child: Container(
+                    height: 420,
+                    width: MediaQuery.of(context).size.width,
+                    decoration:  BoxDecoration(
+                      borderRadius:const  BorderRadius.only(
+                        topLeft: Radius.circular(30),
+                        topRight: Radius.circular(30),
+                        bottomRight: Radius.circular(30),
+                        bottomLeft: Radius.circular(30),
+                      ),
+                      gradient: LinearGradient(
+                        begin: Alignment.bottomLeft,
+                        end: Alignment.topRight,
+                        colors: [
+                          Colors.blue.withOpacity(0.6),
+                          Colors.blueAccent.withOpacity(0.4),
+                        ]
+                      )
+                    ),
+                    child: Center(
+                      child: Container(
+                        margin: const EdgeInsets.only(
+                          right: 10,
+                          left: 10,
+                          top: 10,
+                          bottom: 10,
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                           const  Center(
+                              child: CircleAvatar(
+                                radius: 70,
+                                backgroundColor: Colors.white,
+                                backgroundImage: AssetImage('Images/lost.jpg',),
+                              ),
+                            ),
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text("Category:", style: TextStyle(color: Colors.black,
+                                    fontSize: 25,fontWeight: FontWeight.bold),),
+                                const SizedBox(width: 20,),
+                                Text(document['Category'],
+                                  style: const TextStyle(color: Colors.black54,
+                                      fontWeight: FontWeight.normal,fontSize: 20),),
+                              ],
+                            ),
+                            Space(),
+                            Wrap(
+                              children: [
+                                const Text("Description:", style: TextStyle(color: Colors.black,
+                                    fontSize: 25,fontWeight: FontWeight.bold),),
+                                const SizedBox(width: 20,),
+                                Text(document['Item Description'],
+                                  style: const TextStyle(color: Colors.black54,
+                                      fontWeight: FontWeight.normal,fontSize: 20),),
+                              ],
+                            ),
+
+                            Space(),
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text("Item Name:", style: TextStyle(color: Colors.black,
+                                    fontSize: 25,fontWeight: FontWeight.bold),),
+                                const SizedBox(width: 20,),
+                                Text(document['Item Name'],
+                                  style: const TextStyle(color: Colors.black54,
+                                      fontWeight: FontWeight.normal,fontSize: 20),),
+                              ],
+                            ),
+                            Space(),
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text("Lost Within:", style: TextStyle(color: Colors.black,
+                                    fontSize: 25,fontWeight: FontWeight.bold),),
+                                const SizedBox(width: 20,),
+                                Text(document['Location'],
+                                  style: const TextStyle(color: Colors.black54,
+                                      fontWeight: FontWeight.normal,fontSize: 20),),
+                              ],
+                            ),
+                            Space(),
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text("Mising Date:", style: TextStyle(color: Colors.black,
+                                    fontSize: 25,fontWeight: FontWeight.bold),),
+                                const SizedBox(width: 20,),
+                                Text(document['Mising Date'],
+                                  style: const TextStyle(color: Colors.black54,
+                                      fontWeight: FontWeight.normal,fontSize: 20),),
+                              ],
+                            ),
+
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+
+              }).toList(),
+            );
+          }
+          ),
+      ),
+    );
+  }
+}
--- lib/LostItems/Lost.dart
+++ lib/LostItems/Lost.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:lostfound/Authentication/reusableWidgets1.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:lostfound/LostItems/FetchlostItems.dart';
+import 'package:lostfound/LostItems/LostitemsAdmin.dart';
 class Lost extends StatefulWidget {
   const Lost({Key? key}) : super(key: key);
 
@@ -8,11 +11,20 @@ class Lost extends StatefulWidget {
 }
 
 class _LostState extends State<Lost> {
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
+  final categorycontroller = TextEditingController();
+  final namecontroller = TextEditingController();
+  final datecontroller = TextEditingController();
+  final locationcontroller = TextEditingController();
+  final descrptioncontroller = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Lost Page"),
+        title: const Text("LOST ITEMS",
+        style: TextStyle(color: Colors.white,
+            fontWeight: FontWeight.bold,fontSize: 30),
+        ),
         centerTitle: true,
       ),
       body: SafeArea(
@@ -26,107 +38,144 @@ class _LostState extends State<Lost> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children:  [
-                Bigtext(text: "Have you Lost an item an you want to Find it?"),
+                Bigtext(text: "Have you Lost an item and you want to Find it?"),
                 Space(),
                 Smalltext(text: "Fill in the details about the Item in the fields below"),
                 const SizedBox(height: 30,),
                  TextFormField(
+                   controller: categorycontroller,
                   keyboardType: TextInputType.text,
                   maxLines: 1,
+                  maxLength: 10,
                   decoration:  InputDecoration(
                     border: OutlineInputBorder(
                       borderSide: const BorderSide(
                         width: 2,
                         color: Colors.green,
                       ),
-                      borderRadius: BorderRadius.circular(20)
+                      borderRadius: BorderRadius.circular(10)
                     ),
                     label: const Text("Item Category"),
                   ),
                 ),
                 const SizedBox(height: 10,),
                 TextFormField(
+                  controller: namecontroller,
                   keyboardType: TextInputType.text,
                   maxLines: 1,
+                  maxLength: 10,
                   decoration:  InputDecoration(
                     border: OutlineInputBorder(
                         borderSide: const BorderSide(
                           width: 2,
                           color: Colors.green,
                         ),
-                        borderRadius: BorderRadius.circular(20)
+                        borderRadius: BorderRadius.circular(10)
                     ),
                     label: const Text("Item Name"),
                   ),
                 ),
                 const SizedBox(height: 10,),
                  TextFormField(
+                   controller: datecontroller,
                   keyboardType: TextInputType.datetime,
                   maxLines: 1,
+                  maxLength: 10,
                   decoration:  InputDecoration(
                     border: OutlineInputBorder(
                         borderSide: const BorderSide(
                           width: 2,
                           color: Colors.green,
                         ),
-                        borderRadius: BorderRadius.circular(20)
+                        borderRadius: BorderRadius.circular(10)
                     ),
                     label: const Text("Missing Date"),
                   ),
                 ),
                 const SizedBox(height: 10,),
                 TextFormField(
+                  controller: locationcontroller,
                   keyboardType: TextInputType.text,
                   maxLines: 1,
+                  maxLength: 10,
                   decoration:  InputDecoration(
                     border: OutlineInputBorder(
                         borderSide: const BorderSide(
                           width: 2,
                           color: Colors.green,
                         ),
-                        borderRadius: BorderRadius.circular(20)
+                        borderRadius: BorderRadius.circular(10)
                     ),
                     label:const  Text("Location"),
                   ),
                 ),
                 const SizedBox(height: 10,),
                  TextFormField(
+                   controller: descrptioncontroller,
                   keyboardType: TextInputType.text,
                   maxLines: 4,
+                  maxLength: 100,
                   decoration:  InputDecoration(
                     border: OutlineInputBorder(
                         borderSide: const BorderSide(
                           width: 2,
                           color: Colors.green,
                         ),
-                        borderRadius: BorderRadius.circular(20)
+                        borderRadius: BorderRadius.circular(10)
                     ),
                     label: const Text("Description of the Lost item"),
                   ),
                 ),
                 const SizedBox(height: 10,),
-                TextFormField(
-                  keyboardType: TextInputType.datetime,
-                  maxLines: 4,
-                  decoration:  InputDecoration(
-                    border: OutlineInputBorder(
-                        borderSide: const BorderSide(
-                          width: 2,
-                          color: Colors.green,
-                        ),
-                        borderRadius: BorderRadius.circular(20)
-                    ),
-                    label: const  Text("Any other relevant infomation about the  Item"),
-                  ),
-                ),
-                const SizedBox(height: 30,),
                 Container(
                   height: 50,
                   width: MediaQuery.of(context).size.width,
                   child: ElevatedButton(
-                    onPressed: (){},
+                    onPressed: ()async{
+                      if(
+                      categorycontroller.text.isEmpty||
+                          namecontroller.text.isEmpty||
+                      datecontroller.text.isEmpty||
+                      locationcontroller.text.isEmpty||
+                          descrptioncontroller.text.isEmpty
+                      ){
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            duration: Duration(seconds: 2),
+                              content: Text("Please fill all fields",
+                            style: TextStyle(color: Colors.red),
+                          )),
+                        );
+                      }else{
+                        await firestore.collection('Lost_items').doc().set({
+                          'Category': categorycontroller.text,
+                          'Item Name': namecontroller.text,
+                          'Mising Date': datecontroller.text.toString(),
+                          'Location': locationcontroller.text,
+                          "Item Description": descrptioncontroller.text,
+                        });
+                      }
+
+                    },
                     child: const Text("Submit Details",
                     style: TextStyle(color: Colors.white,fontSize: 30,fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+                Space(),
+                Container(
+                  height: 50,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    color: Colors.purple.withOpacity(0.3),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: ElevatedButton(
+                    onPressed: (){
+                      Navigator.of(context).push(MaterialPageRoute(builder: (context)=>const lostAdmin()));
+                    },
+                    child: const Text("Access Lost Items",
+                      style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 30,),
                     ),
                   ),
                 ),
