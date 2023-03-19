818c3f2a8d0d2cc987e2724d2b007cb7b561ab17
Emilio Kariuki <emilio113kariuki@gmail.com>
2023-03-19 13:53:58 +0300

    FIX: refactored the code to pick an image and upload the code to firebase

--- lib/LostItems/Lost.dart
+++ lib/LostItems/Lost.dart
@@ -1,12 +1,12 @@
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:lostfound/Authentication/reusableWidgets1.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:lostfound/Imagepic/Imagepick.dart';
 import 'package:lostfound/LostItems/LostitemsAdmin.dart';
 import 'dart:io';
 import 'package:image_picker/image_picker.dart';
-import 'package:firebase_storage/firebase_storage.dart' as storage;
-import 'package:path/path.dart';
+import 'package:path/path.dart' as path;
+
 class Lost extends StatefulWidget {
   const Lost({Key? key}) : super(key: key);
 
@@ -15,82 +15,78 @@ class Lost extends StatefulWidget {
 }
 
 class _LostState extends State<Lost> {
-  storage.FirebaseStorage Storage = storage.FirebaseStorage.instance;
-  File? _photo;
-  final ImagePicker _picker = ImagePicker();
   final FirebaseFirestore firestore = FirebaseFirestore.instance;
   final categorycontroller = TextEditingController();
   final namecontroller = TextEditingController();
   final datecontroller = TextEditingController();
   final locationcontroller = TextEditingController();
   final descrptioncontroller = TextEditingController();
-  Future picfromgallery()async{
-    final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
-    setState(() {
-      if(pickedFile != null){
-        _photo = File(pickedFile.path);
-        uploadFile();
-      } else{
-        print("No image selected");
+  //* the link to the image
+  String? url;
+  File? _photo;
+  final ImagePicker _picker = ImagePicker();
+
+  //* refactored code for imagep picker
+  getImage(ImageSource source) async {
+    try {
+      final image = await _picker.pickImage(
+          source: source, imageQuality: 70,);
+      if (image != null) {
+        _photo = File(image.path);
+        setState(() {     
+        });
       }
-    });
-  }
-  Future picFromcamera()async{
-    final pickedFile = await _picker.pickImage(source: ImageSource.camera);
-    setState(() {
-      if(pickedFile != null){
-        _photo = File(pickedFile.path);
-        uploadFile();
-      } else{
-        print("No image selected");
-      }
-    });
-  }
-  Future uploadFile()async{
-    if(_photo==null) return;
-    final Filename = basename(_photo!.path);
-    final destination = 'files/$Filename';
-    try{
-      final ref = storage.FirebaseStorage.instance
-          .ref(destination)
-          .child('file/');
-      await ref.putFile(_photo!);
-    }
-    catch(e){
-      print("No file selected");
+    } catch (e) {
+      debugPrint(e.toString());
     }
   }
+
+  //* refactored code to upload image to firebase storage and get the download url
+  uploadImage(File image) async {
+    try {
+      FirebaseStorage storage = FirebaseStorage.instance;
+      String fileName = path.basename(image.path);
+      Reference ref = storage.ref().child('lost/$fileName');
+      await ref.putFile(image);
+      url = await ref.getDownloadURL();
+      return url;
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("LOST ITEMS",
-        style: TextStyle(color: Colors.white,
-            fontWeight: FontWeight.bold,fontSize: 30),
+        title: const Text(
+          "LOST ITEMS",
+          style: TextStyle(
+              color: Colors.white, fontWeight: FontWeight.bold, fontSize: 30),
         ),
         centerTitle: true,
       ),
       body: SafeArea(
         child: SingleChildScrollView(
           child: Container(
-            margin:const  EdgeInsets.only(
+            margin: const EdgeInsets.only(
               top: 30,
               left: 20,
               right: 20,
             ),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
-              children:  [
+              children: [
                 Bigtext(text: "Have you Lost an item and you want to Find it?"),
                 Space(),
-                Smalltext(text: "Fill in the details about the Item in the fields below"),
+                Smalltext(
+                    text:
+                        "Fill in the details about the Item in the fields below"),
                 Space(),
                 Center(child: Bigtext(text: "Upload lost Item")),
                 Container(
-                  padding: const  EdgeInsets.only(
-                      top: 40,left: 30,right: 30
-                  ),
-                  child:Column(
+                  padding: const EdgeInsets.only(top: 40, left: 30, right: 30),
+                  child: Column(
                     children: <Widget>[
                       const SizedBox(
                         height: 32,
@@ -105,138 +101,146 @@ class _LostState extends State<Lost> {
                             backgroundColor: const Color(0xffFDCF09),
                             child: _photo != null
                                 ? ClipRRect(
-                              borderRadius: BorderRadius.circular(50),
-                              child: Image.file(
-                                _photo!,
-                                width: 100,
-                                height: 100,
-                                fit: BoxFit.cover,
-                              ),
-                            )
+                                    borderRadius: BorderRadius.circular(50),
+                                    child: Image.file(
+                                      _photo!,
+                                      width: 100,
+                                      height: 100,
+                                      fit: BoxFit.cover,
+                                    ),
+                                  )
                                 : Container(
-                              decoration: BoxDecoration(
-                                  color: Colors.grey[200],
-                                  borderRadius: BorderRadius.circular(50)),
-                              width: 100,
-                              height: 100,
-                              child: Icon(
-                                Icons.camera_alt,
-                                color: Colors.grey[800],
-                              ),
-                            ),
+                                    decoration: BoxDecoration(
+                                        color: Colors.grey[200],
+                                        borderRadius:
+                                            BorderRadius.circular(50)),
+                                    width: 100,
+                                    height: 100,
+                                    child: Icon(
+                                      Icons.camera_alt,
+                                      color: Colors.grey[800],
+                                    ),
+                                  ),
                           ),
                         ),
                       )
                     ],
                   ),
                 ),
-                const SizedBox(height: 30,),
-                 TextFormField(
-                   controller: categorycontroller,
+                const SizedBox(
+                  height: 30,
+                ),
+                TextFormField(
+                  controller: categorycontroller,
                   keyboardType: TextInputType.text,
                   maxLines: 1,
                   maxLength: 10,
-                  decoration:  InputDecoration(
+                  decoration: InputDecoration(
                     border: OutlineInputBorder(
-                      borderSide: const BorderSide(
-                        width: 2,
-                        color: Colors.green,
-                      ),
-                      borderRadius: BorderRadius.circular(10)
-                    ),
+                        borderSide: const BorderSide(
+                          width: 2,
+                          color: Colors.green,
+                        ),
+                        borderRadius: BorderRadius.circular(10)),
                     label: const Text("Item Category"),
                   ),
                 ),
-                const SizedBox(height: 10,),
+                const SizedBox(
+                  height: 10,
+                ),
                 TextFormField(
                   controller: namecontroller,
                   keyboardType: TextInputType.text,
                   maxLines: 1,
                   maxLength: 10,
-                  decoration:  InputDecoration(
+                  decoration: InputDecoration(
                     border: OutlineInputBorder(
                         borderSide: const BorderSide(
                           width: 2,
                           color: Colors.green,
                         ),
-                        borderRadius: BorderRadius.circular(10)
-                    ),
+                        borderRadius: BorderRadius.circular(10)),
                     label: const Text("Item Name"),
                   ),
                 ),
-                const SizedBox(height: 10,),
-                 TextFormField(
-                   controller: datecontroller,
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  controller: datecontroller,
                   keyboardType: TextInputType.datetime,
                   maxLines: 1,
                   maxLength: 10,
-                  decoration:  InputDecoration(
+                  decoration: InputDecoration(
                     border: OutlineInputBorder(
                         borderSide: const BorderSide(
                           width: 2,
                           color: Colors.green,
                         ),
-                        borderRadius: BorderRadius.circular(10)
-                    ),
+                        borderRadius: BorderRadius.circular(10)),
                     label: const Text("Missing Date"),
                   ),
                 ),
-                const SizedBox(height: 10,),
+                const SizedBox(
+                  height: 10,
+                ),
                 TextFormField(
                   controller: locationcontroller,
                   keyboardType: TextInputType.text,
                   maxLines: 1,
                   maxLength: 10,
-                  decoration:  InputDecoration(
+                  decoration: InputDecoration(
                     border: OutlineInputBorder(
                         borderSide: const BorderSide(
                           width: 2,
                           color: Colors.green,
                         ),
-                        borderRadius: BorderRadius.circular(10)
-                    ),
-                    label:const  Text("Location"),
+                        borderRadius: BorderRadius.circular(10)),
+                    label: const Text("Location"),
                   ),
                 ),
-                const SizedBox(height: 10,),
-                 TextFormField(
-                   controller: descrptioncontroller,
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  controller: descrptioncontroller,
                   keyboardType: TextInputType.text,
                   maxLines: 4,
                   maxLength: 100,
-                  decoration:  InputDecoration(
+                  decoration: InputDecoration(
                     border: OutlineInputBorder(
                         borderSide: const BorderSide(
                           width: 2,
                           color: Colors.green,
                         ),
-                        borderRadius: BorderRadius.circular(10)
-                    ),
+                        borderRadius: BorderRadius.circular(10)),
                     label: const Text("Description of the Lost item"),
                   ),
                 ),
-                const SizedBox(height: 10,),
-                Container(
+                const SizedBox(
+                  height: 10,
+                ),
+                SizedBox(
                   height: 50,
                   width: MediaQuery.of(context).size.width,
                   child: ElevatedButton(
-                    onPressed: ()async{
-                      if(
-                      categorycontroller.text.isEmpty||
-                          namecontroller.text.isEmpty||
-                      datecontroller.text.isEmpty||
-                      locationcontroller.text.isEmpty||
-                          descrptioncontroller.text.isEmpty
-                      ){
+                    onPressed: () async {
+                      if (categorycontroller.text.isEmpty ||
+                          namecontroller.text.isEmpty ||
+                          datecontroller.text.isEmpty ||
+                          locationcontroller.text.isEmpty ||
+                          descrptioncontroller.text.isEmpty) {
                         ScaffoldMessenger.of(context).showSnackBar(
                           const SnackBar(
-                            duration: Duration(seconds: 2),
-                              content: Text("Please fill all fields",
-                            style: TextStyle(color: Colors.red),
-                          )),
+                              duration: Duration(seconds: 2),
+                              content: Text(
+                                "Please fill all fields",
+                                style: TextStyle(color: Colors.red),
+                              )),
                         );
-                      }else{
+                      } else {
                         await firestore.collection('Lost_items').doc().set({
+                          "Image": url,
                           'Category': categorycontroller.text,
                           'Item Name': namecontroller.text,
                           'Mising Date': datecontroller.text.toString(),
@@ -244,10 +248,13 @@ class _LostState extends State<Lost> {
                           "Item Description": descrptioncontroller.text,
                         });
                       }
-
                     },
-                    child: const Text("Submit Details",
-                    style: TextStyle(color: Colors.white,fontSize: 30,fontWeight: FontWeight.bold),
+                    child: const Text(
+                      "Submit Details",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold),
                     ),
                   ),
                 ),
@@ -260,11 +267,17 @@ class _LostState extends State<Lost> {
                     borderRadius: BorderRadius.circular(10),
                   ),
                   child: ElevatedButton(
-                    onPressed: (){
-                      Navigator.of(context).push(MaterialPageRoute(builder: (context)=>const lostAdmin()));
+                    onPressed: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => const lostAdmin()));
                     },
-                    child: const Text("Access Lost Items",
-                      style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 30,),
+                    child: const Text(
+                      "Access Lost Items",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30,
+                      ),
                     ),
                   ),
                 ),
@@ -275,31 +288,33 @@ class _LostState extends State<Lost> {
       ),
     );
   }
+
   void _showPicker(context) {
     showModalBottomSheet(
         context: context,
         builder: (BuildContext bc) {
           return SafeArea(
-            child: Container(
-              child: Wrap(
-                children: <Widget>[
-                  ListTile(
-                      leading: const Icon(Icons.photo_library),
-                      title: const  Text('Gallery'),
-                      onTap: () {
-                        picfromgallery();
-                        Navigator.of(context).pop();
-                      }),
-                  ListTile(
-                    leading:  const Icon(Icons.photo_camera),
-                    title: const Text('Camera'),
-                    onTap: () {
-                      picFromcamera();
+            child: Wrap(
+              children: <Widget>[
+                ListTile(
+                    leading: const Icon(Icons.photo_library),
+                    title: const Text('Gallery'),
+                    onTap: () async {
+                      await getImage(ImageSource.gallery);
+                      await uploadImage(_photo!);
                       Navigator.of(context).pop();
-                    },
-                  ),
-                ],
-              ),
+                    }),
+                ListTile(
+                  leading: const Icon(Icons.photo_camera),
+                  title: const Text('Camera'),
+                  onTap: () async {
+                    await getImage(ImageSource.camera).then((value) {
+                      uploadImage(_photo!);
+                    });
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
             ),
           );
         });
